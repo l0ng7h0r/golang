24e039cccb4c1bc26e58ed330493d39ba50dd2e4
Bounluang THOR <bounluangt@gmail.com>
2026-04-11 23:16:34 +0700

    update folder

--- shop_online/internal/domain/category.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type Category struct {
-	ID       string  `json:"id"`
-	Name     string  `json:"name"`
-	ParentID *string `json:"parent_id"` // pointer to allow null for root categories
-}
